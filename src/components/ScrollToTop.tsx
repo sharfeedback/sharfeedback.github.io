3f57b11071ff1c192be792a7f83c5abe7750e212
Shashank Holla <sholla@nvidia.com>
2025-01-13 13:44:43 +0530

    Updates

--- src/components/ScrollToTop.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-const ScrollToTop = () => {
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, []); // Empty dependency array to run only on mount
-  return null; // This component doesn't render anything
-};
-
-export default ScrollToTop;
